b708b5c02f53a9d946e64aa213a3f0e32ff4aee5
grgttdln <georgettedalen@gmail.com>
2026-01-06 16:35:45 +0800

    feat: initial commit

---
+++ Track/TrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackApp.swift
+//  Track
+//
+//  Created by Georgette Dalen on 1/6/26.
+//
+
+import SwiftUI
+
+@main
+struct TrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
